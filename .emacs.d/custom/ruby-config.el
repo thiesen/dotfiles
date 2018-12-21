e4c6a6a5350c323ccefa2ab59159fdaa5204521d
thiesen <nathan.thiesen@marleyspoon.com>
2018-12-21 14:18:43 +0100

    remove enh-ruby-mode faces

--- .emacs.d/custom/ruby-config.el
+++ .emacs.d/custom/ruby-config.el
@@ -10,3 +10,5 @@
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist
              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+
+(remove-hook 'enh-ruby-mode-hook 'erm-define-faces)
