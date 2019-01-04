e0b64cdee5e1354886a2d8255bf4024e182eff72
thiesen <nathanpthiesen@gmail.com>
2019-01-04 17:58:27 +0100

    add .tmpl to web mode list

--- .emacs.d/custom/web-config.el
+++ .emacs.d/custom/web-config.el
@@ -7,6 +7,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tmpl\\'" . web-mode))
 
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
